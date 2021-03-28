cf3e47e9f2a82aa126299ee35cf4e60a102ce4a3
Marcin <stachukolo@gmail.com>
2021-03-28 15:48:59 +0200

    zadanie124

---
+++ pole_prostokąta.c
@@ -0,0 +1,23 @@
+/*Marcin Staśkiewicz Zadanie1*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+    float a;
+    float b;
+    float pole_prostokata;
+
+    printf("podaj_a:");
+    scanf("%f:", &b);
+
+    printf("podaj_b:");
+    scanf("%f:", &a);
+
+    pole_prostokata = (a*b);
+    printf("pole prostokąta wynosi : %f", pole_prostokata);
+    return 0;
+}
